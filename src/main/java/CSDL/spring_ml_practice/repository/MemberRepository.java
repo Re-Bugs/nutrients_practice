217a084f108bf3a92ed9db6c161e327b4a046461
Lee SeungWook <leesw1998@gmail.com>
2024-06-30 05:37:43 +0900

    멤버 리포지토리 수정

--- src/main/java/CSDL/spring_ml_practice/repository/MemberRepository.java
+++ src/main/java/CSDL/spring_ml_practice/repository/MemberRepository.java
@@ -3,7 +3,7 @@ package CSDL.spring_ml_practice.repository;
 import CSDL.spring_ml_practice.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, String> {
     Member findByMemberEmailAndPassword(String memberEmail, String password);
     Member findByMemberEmail(String memberEmail); // 메서드 추가
 }
